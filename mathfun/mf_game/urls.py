243a6a3d132b35c5d32b7976ce55490f767aff7f
Amine Haddad <aminehaddad@gmail.com>
2016-12-27 15:35:14 -0500

    Landing page now redirects to the game.

--- mathfun/mf_game/urls.py
+++ mathfun/mf_game/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.landing, name="landing"),
     url(r'^game/$', views.game, name="game"),
 ]
--- mathfun/mf_game/views.py
+++ mathfun/mf_game/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+def landing(request):
+    return HttpResponseRedirect(reverse("game"))
 
 def game(request, template="mf_game/game.html"):
     data = {}
